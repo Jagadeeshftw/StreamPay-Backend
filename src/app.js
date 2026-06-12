bc47035c9c89e67800d512b11c32c4addd2a8220
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:26:27 +0530

    feat(app): cap JSON request body size at 100kb

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function createApp() {
 
   app.use(cors());
   app.use(securityHeaders);
-  app.use(express.json());
+  app.use(express.json({ limit: '100kb' }));
   app.use(morgan(config.env === 'development' ? 'dev' : 'combined'));
   app.use(requestLogger);
 
