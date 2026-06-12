f3a8d6100e32efa0f3b29553a0e3e32b3b7c6be9
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:25:25 +0530

    feat(routes): validate withdraw payload on withdraw route

--- src/routes/streamRoutes.js
+++ src/routes/streamRoutes.js
@@ -4,14 +4,14 @@ const { Router } = require('express');
 const streamController = require('../controllers/streamController');
 const validate = require('../middleware/validate');
 const asyncHandler = require('../utils/asyncHandler');
-const { validateCreateStream } = require('../validators/streamValidators');
+const { validateCreateStream, validateWithdraw } = require('../validators/streamValidators');
 
 const router = Router();
 
 router.post('/streams', validate(validateCreateStream), asyncHandler(streamController.create));
 router.get('/streams', streamController.list);
 router.get('/streams/:id', streamController.getById);
-router.post('/streams/:id/withdraw', asyncHandler(streamController.withdraw));
+router.post('/streams/:id/withdraw', validate(validateWithdraw), asyncHandler(streamController.withdraw));
 router.post('/streams/:id/cancel', asyncHandler(streamController.cancel));
 
 module.exports = router;
