4ccb656f4720fc78834c0bae5c6a2eeb95d6d423
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:26:37 +0530

    feat(routes): guard stream id param format

--- src/routes/streamRoutes.js
+++ src/routes/streamRoutes.js
@@ -4,9 +4,18 @@ const { Router } = require('express');
 const streamController = require('../controllers/streamController');
 const validate = require('../middleware/validate');
 const asyncHandler = require('../utils/asyncHandler');
+const ApiError = require('../utils/ApiError');
 const { validateCreateStream, validateWithdraw } = require('../validators/streamValidators');
 
 const router = Router();
+
+// Guard the :id param so obviously malformed ids short-circuit to a 400.
+router.param('id', (req, res, next, id) => {
+  if (typeof id !== 'string' || !id.startsWith('stream_')) {
+    return next(ApiError.badRequest('Invalid stream id'));
+  }
+  next();
+});
 
 router.post('/streams', validate(validateCreateStream), asyncHandler(streamController.create));
 router.get('/streams', streamController.list);
